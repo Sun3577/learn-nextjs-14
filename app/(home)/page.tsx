95ac0298c1ef42f902222af89fd80479c40a0a7f
Sun3577 <151902933+Sun3577@users.noreply.github.com>
2024-04-07 01:01:09 +0900

    Metadata

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,10 @@
-import Navigation from "./components/navigation";
+export const metadata = {
+  title: "Home | Next Movies",
+};
 
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>Hello</h1>
     </div>
   );
